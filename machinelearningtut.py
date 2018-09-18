085b1a6973e75b5355f9f218d96770fc2b10802e
Fluxstone <yannick-borschneck@outlook.de>
2018-09-18 22:46:05 +0200

    3rd commit

--- machinelearningtut.py
+++ machinelearningtut.py
@@ -1,1 +1,17 @@
-print("hello")
+list(range(10))
+val = 10
+n = 1
+for n in range(n, n+val ):
+    print("Nr. " ,n , "Squareroot", n*n)
+    pass
+print("Done")
+
+def test(a, b):
+    val = a+b
+    return val
+
+t1 = 3
+t2 = 6
+
+t3 = test(t1, t2)
+print(t3)
