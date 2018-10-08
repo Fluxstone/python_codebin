563747d4ef18b0dc220898cc8a71f836c6329f06
Fluxstone <yannick-borschneck@outlook.de>
2018-10-08 18:08:21 +0200

    Desktop commit

--- Numpy.py
+++ Numpy.py
@@ -1,1 +1,14 @@
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.zeros( [3,2] )
+
+a[0,0] = 3
+a[0,1] = 7
+a[1,0] = 1
+a[2,1] = 4
+
+plt.imshow(a, interpolation="nearest")
+plt.show()
+
+print(a)
---
+++ object_tut.py
@@ -0,0 +1,8 @@
+class Dog:
+    def bark(self):
+        print("Russian Woof blyat")
+        pass
+    pass
+
+Ivan = Dog()
+Ivan.bark()
